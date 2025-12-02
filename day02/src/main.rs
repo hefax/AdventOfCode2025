5de9641b817e6434eafae48bd8596a22af4836c7
Johannes Korpela <johannes@silverbit.fi>
2025-12-02 20:20:47 +0200

    Day2: Well it works and Rust is always pretty.. The 2nd part is not the most effective code and it took around 12minutes to resolve.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{self,BufRead};
 use std::path::Path;
 use std::str::FromStr;
+use regex::Regex;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
 where P: AsRef<Path>, {
@@ -30,12 +31,43 @@ fn get_file_data(filename:&str) -> String {
 
 fn check_code(id:i64) -> bool {
     
-    let foo = id.to_str();
+    let foo = format!("{id}");
+    
+    let len = foo.chars().count();
 
+    if len % 2 != 0 {
+        return false;
+    }
 
+    for i in 0..len/2 {
+        let j = (len/2)+i;
 
+        if foo.chars().nth(i).unwrap() != foo.chars().nth(j).unwrap() {
+            return false;
+        }
+    }
+
+    true 
 }
 
+fn check_code2(id:i64) -> bool {
+    let foo = format!("{id}");
+    
+    let len = foo.chars().count();
+
+    for i in 1..=len/2 {
+        let s = &foo[..i];
+
+        let pattern = format!(r"^({})+$",s);
+        let re = Regex::new(pattern.as_str()).unwrap();
+
+        if re.is_match(&foo) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 
 fn first(filename:&str) {
@@ -59,12 +91,48 @@ fn first(filename:&str) {
         };
         
         println!("Range: {low} - {high}:");
-        for num in low..high {
-            println!("{num}");
+        for num in low..=high {
+            if check_code(num) {
+                println!("{num}");
+                sum += num; 
+            }
         }
     }
 
 
+
+    println!("Final: {sum}");
+
+}
+
+fn second(filename:&str) {
+    let input = get_file_data(filename);
+
+    let mut sum:i64 = 0;
+    
+    let ranges = input.split(",");
+
+    for range in ranges {
+        let ends:Vec<&str> = range.split("-").collect();
+
+
+        let low:i64  = match FromStr::from_str(ends[0].trim()) {
+            Ok(num) => num,
+            Err(_) => panic!("jotain kusi"),
+        };
+        let high:i64  = match FromStr::from_str(ends[1].trim()) {
+            Ok(num) => num,
+            Err(_) => panic!("jotain kusi"),
+        };
+        
+        println!("Range: {low} - {high}:");
+        for num in low..=high {
+            if check_code2(num) {
+                println!("{num}");
+                sum += num; 
+            }
+        }
+    }
 
     println!("Final: {sum}");
 
@@ -75,4 +143,6 @@ fn first(filename:&str) {
 
 fn main() {
     first("test.txt");
+    println!("----------");
+    second("input.txt");
 }
