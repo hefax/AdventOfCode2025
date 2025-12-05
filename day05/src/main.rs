b146990e45217df80765337b59ac58d8f3d3a87a
Johannes Korpela <johannes@silverbit.fi>
2025-12-05 12:33:02 +0200

    Day 5: This was a fucking chore with rust. I think there should have been a prettier and better way to solve this but I was not feeling smart today

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -44,7 +44,6 @@ fn first(filename:&str) {
     let input = get_file_data(filename);
 
     let mut ranges:Vec<(i64,i64)> = vec![];
-    let mut product:Vec<i64> = vec![];
     
     let mut state="ranges";
 
@@ -101,10 +100,10 @@ fn first(filename:&str) {
 }
 
 
-fn check_range(low:i64,high:i64,ranges:mut &Vec<(i64,i64)>) -> bool{
+fn check_range(low:i64,high:i64,ranges:&Vec<(i64,i64)>) -> Option<Vec<(i64,i64)>> {
 
     let mut low_t = low;
-    let mut high_t = low;
+    let mut high_t = high;
 
     // set our ends in suitable distances. 
     for range in ranges {
@@ -117,14 +116,14 @@ fn check_range(low:i64,high:i64,ranges:mut &Vec<(i64,i64)>) -> bool{
         if high_t >= range.0 && high_t <= range.1 {
             // our high_range is in this range. 
             // we need to set our high end to the low end of this range.
-            low_t = range.0-1;
+            high_t = range.0-1;
         }
 
     }
         
 
     if low_t > high_t {
-        return false;
+        return None;
     }
     // we now have a range that is not conflicting on it's upper or lower bounds.
     // last thing to do is to check if there are smaller 
@@ -132,35 +131,134 @@ fn check_range(low:i64,high:i64,ranges:mut &Vec<(i64,i64)>) -> bool{
     //
 
     let mut gaps:Vec<(i64,i64)> = vec![];
-    let mut gaps:Vec<(i64,i64)> = vec![];
+    gaps.push((low_t,high_t));
 
-    let mut gl:i64=-1;
-    let mut gh:i64=-1;
+    
+    if ranges.len() == 0 {
+        return Some(gaps);
+    }
 
-    for range in ranges {
-        if  range.0 >= low_t  && range.0 <= high {
-            //this range low end is in our range .
-            gl=range.0;
+    loop {
+        let mut split = false;
+        let my_range = gaps.pop();
+
+        match my_range {
+            Some(my_range) => {
+                let mut tmp:Vec<(i64,i64)> = vec![];
+                tmp.push(my_range);
+
+                // shoot holes. into the range:
+                for range in ranges {
+                    let splits = inset_range(&tmp,&range);
+
+                    if splits.len() > 1 {
+                        split=true;
+                    }
+
+                    tmp = splits;
+
+                }
+
+                // not sure if duplicate filtering is needed anymore.
+                for item in tmp {
+                    let fo = gaps.iter().find(|&&val| val==item);
+
+                    match fo {
+                        Some(_) => { 
+                            // we already have this;
+                        },
+                        None => {
+                            gaps.push((item.0,item.1));
+                        }
+                    }
+                }
+
+            },
+            None => {
+            },
         }
 
-        if  range.1 >= low_t  && range.1 <= high_t {
-            // thie range high end is in our range.
-            gh=range.1;
-            assert!((gl > -1));
+        if split == false {
+            break;
         }
-
-        // note both of these should be set 
-        //
-
-
-
+    }
         
+
+    Some(gaps)
+}
+
+fn inset_range(my_range:&Vec<(i64,i64)>,existing:&(i64,i64)) -> Vec<(i64,i64)> {
+    let mut set:Vec<(i64,i64)> = vec![];
+    let mut result:Vec<(i64,i64)> = vec![];
+
+
+    for item in my_range {
+        set.push((item.0,item.1));
+    }
+    let low_x = existing.0;
+    let high_x = existing.1;
+
+
+    loop {
+
+        let range = set.pop();
+
+        match range {
+            Some(range) => {
+                let mut low= range.0;
+                let mut high= range.1;
+                
+                if high_x < low {
+                    // the existing does no affect this part. 
+                    result.push((low,high));
+                    continue;
+                }
+
+                if low_x > high {
+                    // the existing does no affect this part.
+                    result.push((low,high));
+                    continue;
+                }
+
+                if low_x > low && high_x < high {
+                    // the existing is in the middle of this part. 
+                    // lets just add this here 
+                    set.push((low,low_x-1));
+                    set.push((high_x+1,high));
+                    continue;
+                }
+
+
+                if low_x == low && high_x < high {
+                    low = high_x+1; 
+                    set.push((low,high));
+                    continue;
+
+                }
+
+                if low_x > low && high_x == high {
+                    high = low_x-1;
+                    set.push((low,high));
+                    continue;
+
+                }
+
+
+                panic!("this should not be.");
+            },
+            None => {
+                break;
+            },
+        }
 
     }
 
-    false
-}
 
+ //                    println!("{:?} - {:?} -> {:?}",my_range,existing,result);
+
+
+    result
+}
 
 
 
@@ -169,16 +267,12 @@ fn second(filename:&str) {
     let input = get_file_data(filename);
 
     let mut ranges:Vec<(i64,i64)> = vec![];
-    let mut product:Vec<i64> = vec![];
-    
-    let mut state="ranges";
 
     let mut fresh:i64 = 0;
 
     for (_,rivi) in input.lines().enumerate() {
 
         if rivi.trim().len() == 0 {
-            state="products";
             break;
         }
         else {
@@ -202,64 +296,27 @@ fn second(filename:&str) {
 
     for range in ranges {
         // 
-        let mut low = range.0;
-        let mut high = range.1;
-
-        let mut low_t:i64 = -1;
-        let mut high_t:i64 = -1;
+        let  low = range.0;
+        let  high = range.1;
 
         println!("Checking {low}-{high}");
-        for i in low..=high {
-            //print!("checking {i}");
-            if check_id(i,&better) {
-                if low_t < high_t {
-                    better.push((low_t,high_t));
+        match check_range(low,high,&better) {
+            Some(list) => {
+                for item in list {
+                    better.push((item.0,item.1));
                 }
-                else {
-                    if check_id(low_t,&better) == false && low_t > -1 {
-                        better.push((low_t,low_t));
-                    }
-                }
-
-                // we
-                low_t=i+1;
-                high_t=i+1;
-                //println!("we were in range so set {low_t} - {high_t}");
             }
-            else {
-                if low_t == -1 {
-                    low_t = i;
-                }
-
-                high_t = i;
-                //println!("we were not in range so set {low_t} - {high_t}");
-            }
+            None => {},
         }
-
-        if low_t < high_t {
-            better.push((low_t,high_t));
-        }
-        else {
-            if check_id(low_t,&better) == false && low_t > -1 {
-                better.push((low_t,low_t));
-            }
-        }
-
-
 
     }
+
 
     // println!("{:?}",better);
 
-    
     for range in better {
-
-        fresh += range.1 - range.0 +1;
-
+        fresh += range.1-range.0+1;
     }
-
-
-
 
 
     println!("Final: {fresh}");
